431b662c3bcbb2caac0fe6511fc70ea1b73fab8a
A00838521 <a00838521@tec.mx>
2024-05-30 15:48:27 -0600

    Update x

--- etapa.R
+++
@@ -1,7 +0,0 @@
-rm(list=ls())
-
-# setwd() Setear un directorio de trabajo
-# getwd() Obtener el directorio de trabajo
-
-datos <- read.csv("G512-datos02.csv")
-write.csv(datos1, file = "datosSalvados.csv")
